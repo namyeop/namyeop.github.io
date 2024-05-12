53bcd93ae835f4b7e900e79de865598b2d06d8d1
Nam Yeop Lee <namyeop@Nams-MacBook-Pro.local>
2024-05-13 00:54:08 +0900

    remove all articles

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Dev, Tech, and Entrepreneurship`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Noam Yeop Lee`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
       twitter: `kylemathews`,
